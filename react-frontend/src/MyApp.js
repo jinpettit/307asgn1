e987846e6699da1c130b1e98c81e1e0ec3929478
Jinrong Pettit <jinpettit@gmail.com>
2023-09-29 22:24:05 -0700

    finished 4

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -1,11 +1,13 @@
 // src/MyApp.js
-import React from "react";
+import React, { useState } from 'react';
 import Table from "./Table";
 
-const characters = [
+
+function MyApp() {
+  const [characters, setCharacters] = useState([
     {
-      name: "Charlie",
-      job: "Janitor",
+      name: 'Charlie',
+      job: 'Janitor',
     },
     {
       name: "Mac",
@@ -19,12 +21,19 @@ const characters = [
       name: "Dennis",
       job: "Bartender",
     },
-  ];
+  ]);
 
-function MyApp() {
-return (
+  function removeOneCharacter(index) {
+    const updated = characters.filter((character, i) => {
+      return i !== index
+    });
+    setCharacters(updated);
+  }
+
+  return (
     <div className="container">
-      <Table characterData={characters} />
+      <Table characterData={characters} 
+        removeCharacter = {removeOneCharacter} />
     </div>
   );
 }
--- react-frontend/src/Table.js
+++ react-frontend/src/Table.js
@@ -5,6 +5,7 @@ function TableHeader() {
         <tr>
           <th>Name</th>
           <th>Job</th>
+          <th>Remove</th>
         </tr>
       </thead>
     );
@@ -14,9 +15,15 @@ function TableBody(props) {
     const rows = props.characterData.map((row, index) => {
       return (
         <tr key={index}>
-          <td>{row.name}</td>
-          <td>{row.job}</td>
-        </tr>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+        <td>
+          <button onClick={() => 
+            props.removeCharacter(index)}>
+            Delete
+          </button>
+        </td>
+      </tr>
       );
      }
     );
@@ -31,7 +38,8 @@ function Table(props) {
     return (
       <table>
         <TableHeader />
-        <TableBody characterData={props.characterData} />
+        <TableBody characterData={props.characterData} 
+          removeCharacter={props.removeCharacter} />
       </table>
     );
 }
