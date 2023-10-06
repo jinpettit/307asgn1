d936640ecf57f445aea369d9cc4f0d14333265a0
Jinrong Pettit <jinpettit@gmail.com>
2023-10-05 18:04:54 -0700

    asng 2 done

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -44,16 +44,75 @@ const users = {
     ]
  }
 
- const findUserById = (id) =>
+const findUserByName = (name) => { 
+   return users['users_list']
+       .filter( (user) => user['name'] === name); 
+};
+
+app.get('/users', (req, res) => {
+   const name = req.query.name;
+   if (name != undefined){
+       let result = findUserByName(name);
+       result = {users_list: result};
+       res.send(result);
+   }
+   else{
+       res.send(users);
+   }
+});
+
+const findUserById = (id) =>
  users['users_list']
      .find( (user) => user['id'] === id);
  
 app.get('/users/:id', (req, res) => {
- const id = req.params['id']; //or req.params.id
- let result = findUserById(id);
- if (result === undefined) {
-     res.status(404).send('Resource not found.');
- } else {
-     res.send(result);
- }
-});
+   const id = req.params['id']; //or req.params.id
+   let result = findUserById(id);
+   if (result === undefined) {
+      res.status(404).send('Resource not found.');
+   } else {
+      res.send(result);
+   }
+   });
+
+const addUser = (user) => {
+   users['users_list'].push(user);
+   return user;
+};
+
+app.post("/users", (req, res) => {
+   const userToAdd = req.body;
+   addUser(userToAdd);
+   res.send();
+ });
+
+const deleteUser = (id) => {
+   return users["users_list"].pop((user) => user["id"] === id);
+ };
+
+app.delete("/users/:id", (req, res) => {
+   const id = req.params["id"];
+   let result = deleteUser(id);
+   if (result === undefined) {
+      res.status(404).send("Resource not found.");
+   } else {
+      res.send(result);
+   }
+   });
+
+const findUserByJobAndName = (job, name) => {
+   return users["users_list"].filter(
+      (user) => user["job"] === job && user["name"] === name
+   );
+   };
+
+app.get("/users", (req, res) => {
+   const name = req.query.name;
+   const job = req.query.job;
+   if (name != undefined && job != undefined) {
+      let result = findUserByJobAndName(job, name);
+      res.send(result);
+   } else {
+      res.status(404).send("Resource not found.");
+   }
+   });
